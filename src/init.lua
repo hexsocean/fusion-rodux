9dae2b48d6ac3140bdedcb908d026cdc2e4f5359
socean <lumengeared@gmail.com>
2021-12-16 16:47:56 +0800

    Made mapStoreToState function optional.

--- src/init.lua
+++ src/init.lua
@@ -1,5 +1,9 @@
 local Fusion: Fusion = require(script.Parent.Fusion)
 
+local function DefaultMapStoreToState(storeState)
+    return storeState
+end
+
 local function NewStoreStateConnection<T>(store: Rodux.Store, fusionState: Fusion.State<T>, mapStoreToState: (any) -> any)
     if not mapStoreToState and typeof(mapStoreToState) ~= "function" then
         error("Must pass in a function to map the store to the fusion state", 2)
@@ -15,7 +19,8 @@ end
 local function ProvideStore(store: Rodux.Store)
     -- Takes in a function to map the store state to a fusion state.
     -- Returns the fusion state in a tuple with a function to disconnect the store from the state.
-    return function <T>(mapStoreToState: (any) -> T): (Fusion.State<T>, () -> nil)
+    return function <T>(mapStoreToState: ((any) -> T)?): (Fusion.State<T>, () -> nil)
+        mapStoreToState = mapStoreToState or DefaultMapStoreToState
         local boundState = Fusion.State()
         local bind = NewStoreStateConnection(store, boundState, mapStoreToState)
         return boundState, bind.disconnect
